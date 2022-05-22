6e36aa900d9a7af7d41e72c4f9c7212986971b73
JunjoFor <castillooton@gmail.com>
2022-05-22 20:51:18 +0200

    intentando arrglar insultos v2

--- listener.py
+++ listener.py
@@ -62,28 +62,27 @@ def listener(update: Update, context: CallbackContext):
     elif 'depression' in message:
         bot.send_photo(chatID, open('./depression.jpg', 'rb'))
     
-    if not user.username == "junjou":
-        if ('junjobot gilipollas' in message or 'gilipollas junjobot' in message):
-            update.effective_message.reply_text('a insultar a tu madre, ' + user.username)
-        elif('adios' in message):
-            update.effective_message.reply_text('bueno adios master')
+    if ('junjobot gilipollas' in message or 'gilipollas junjobot' in message):
+        update.effective_message.reply_text('a insultar a tu madre, ' + user.username)
+    elif('adios' in message):
+        update.effective_message.reply_text('bueno adios master')
 
-        elif ('puta' in words):
-            update.effective_message.reply_text('Puta tu madre')
-        elif ('zorra' in message):
-            update.effective_message.reply_text('Zorra tu madre')
+    elif ('puta' in words):
+        update.effective_message.reply_text('Puta tu madre')
+    elif ('zorra' in message):
+        update.effective_message.reply_text('Zorra tu madre')
 
-        elif ('me cago en' in message):
-            update.effective_message.reply_text('yo me cago en tus muertos pisados a caballo')
-        elif (('warrah' or 'warra') in message and lenWords == 1):
-            update.effective_message.reply_text('warrah')
-        elif ('ano' in words[lenWords - 1][-3:]):
-            update.effective_message.reply_text('Me la agarras con la mano')
-        elif ('cinco' in words[lenWords - 1][-5:]):
-            update.effective_message.reply_text('Por el culo te la hinco')
-        elif ('rita' in words[lenWords - 1][-4:]):
-            update.effective_message.reply_text('La zorrita')
-        elif 'ino' in words[lenWords - 1][-3:]:
-            update.effective_message.reply_text('En tu culo mi pepino')
-        elif 'trece' in words[lenWords - 1][-5:]:
-            update.effective_message.reply_text('Agarramela que me crece')
+    elif ('me cago en' in message):
+        update.effective_message.reply_text('yo me cago en tus muertos pisados a caballo')
+    elif (('warrah' or 'warra') in message and lenWords == 1):
+        update.effective_message.reply_text('warrah')
+    elif ('ano' in words[lenWords - 1][-3:]):
+        update.effective_message.reply_text('Me la agarras con la mano')
+    elif ('cinco' in words[lenWords - 1][-5:]):
+        update.effective_message.reply_text('Por el culo te la hinco')
+    elif ('rita' in words[lenWords - 1][-4:]):
+        update.effective_message.reply_text('La zorrita')
+    elif 'ino' in words[lenWords - 1][-3:]:
+        update.effective_message.reply_text('En tu culo mi pepino')
+    elif 'trece' in words[lenWords - 1][-5:]:
+        update.effective_message.reply_text('Agarramela que me crece')
